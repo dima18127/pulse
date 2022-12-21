a7d4ae4e729b9c61388630279448a842a6907601
dima18127 <dima18127@yandex.ru>
2022-12-21 06:27:38 +0400

    2 src commit arrows red

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/red/left.svg" ></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/red/right.svg" ></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/red/left.svg" ></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/red/right.svg" ></button>',
         responsive: [
             {
                 breakpoint: 1024,
